095e82925da7d0513fcf458bc941e414e300d22a
ChristopheRibault <c.ribault.dev@gmail.com>
2020-11-12 10:15:14 +0100

    chore: setup dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
+
 module.exports = {
 
   development: {
     client: 'mysql',
     connection: {
-      host: 'localhost',
-      database: 'cook',
-      user:     'root',
-      password: 'root',
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: './db/migrations',
